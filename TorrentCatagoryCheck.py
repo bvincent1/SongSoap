3e1c6e8187f96fc88434a93acaa42f9422a00e8c
Ben Vincent <ben@WonkaChan.com>
2014-12-22 21:44:50 -0700

    added catagory module, made stubs for music checks

---
+++ TorrentCatagoryCheck.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# extra libs
+from KickassAPI import CATEGORY, Torrent
+import SongUtil
+
+
+# standard libs
+import time
+import re
+import math
+
+from sys import argv
+from datetime import date
+
+class MusicTorrentCheck:
+	def checkFileNumber(self, torrentCol, targetTor):
+		return
+
+	def performAllChecks(self, torrentCol, targetTor):
+		return
